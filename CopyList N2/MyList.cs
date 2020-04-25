22474ecbe44f0fac61fe0d9bf96904629d8f4f4a
zukaberidze <zaqaria.beridze@sangu.edu.ge>
2020-04-25 23:23:00 +0400

    Add project files.

---
+++ CopyList N2/MyList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyList_N2
+{
+    public class MyList
+    {
+        public int number { get; set; }
+        public MyList(int a) {
+            this.number = a;
+        }
+    }
+}
---
+++ CopyList N2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CopyList_N2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<MyList> N1 = new List<MyList>();
+            N1.Add(new MyList(1));
+            N1.Add(new MyList(2));
+            N1.Add(new MyList(3));
+
+            List<MyList> N2 = new List<MyList>();
+            foreach (var item in N1)
+            {
+                N2.Add(item);
+            }
+
+            foreach (var item in N2)
+            {
+                Console.WriteLine(item.number);
+            }
+                
+        }
+    }
+}
